72aef839f6e32b24d559381c2771b348db2dcce2
Aakash Kumar <aakashkumarp2000@gmail.com>
2025-05-10 05:40:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = require('./src/app')
 
 
 
-app.listen(`https://code-reviewer-ai-frontend-two.vercel.app/`, () => {
+app.listen(3000, () => {
     console.log('Server is running on http://localhost:3000')
 })
